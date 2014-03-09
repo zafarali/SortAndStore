5a6997be7afef23545216e8cda9d99f6ef75b05f
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-03-08 23:32:08 -0500

    removes test cases

--- Search.java
+++ Search.java
@@ -9,10 +9,9 @@ public class Search{
 				return i;
 			}
 		}
-
 		//not found, return a -1!
 		return -1;
-	}
+	}//end linear
 
 	//binary search using loops
 	public static int binary(int[] toSearch, int target){
@@ -38,10 +37,10 @@ public class Search{
 
 		//not found, return a -1!
 		return -1;
-	}
+	}//end binary
 
 	//binary search using recursion
-	//behind the scenes implimentation
+	//behind the scenes implimentation (DO NOT USE)
 	private static int btsrb(int[] a, int t, int l, int r){
 		//escape case:
 		if(l>r) return -1;
@@ -54,22 +53,14 @@ public class Search{
 		if (a[mid] > t)
 			return btsrb(a, t, l, mid-1);
 		return btsrb(a,t,mid+1,r);
-	}
+	}//end btsrb
+
 	//signature for you to use
 	public static int rbinary(int[] toSearch, int target){
 		//initialize the important indexing variables.
 		int left = 0;
 		int right = toSearch.length -1;
 		return btsrb(toSearch, target, left, right);
-	}
+	}//end rbinary
 
-
-	/*public static void main(String[] args){
-		int[] tos = {1, 2, 3, 4};
-		System.out.println(linear(tos, 2));
-		System.out.println(binary(tos, 2));
-		System.out.println(rbinary(tos, 2));
-
-
-	}*/
-}
+}//end class
